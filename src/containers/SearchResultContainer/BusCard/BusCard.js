70ccb780d642c6e3251b3bde2c86bce8411019ea
utpal.sarmah@ap.jll.com <utpal.sarmah@ap.jll.com>
2020-03-12 12:47:42 +0530

    refactoring

--- src/containers/SearchResultContainer/BusCard/BusCard.js
+++ src/containers/SearchResultContainer/BusCard/BusCard.js
@@ -5,62 +5,73 @@ import { Styles } from "./Styles";
 import { DirectionsBus, AccessTime } from "@material-ui/icons";
 
 const BusCard = ({ classes, data: { bus, midId }, handleClick }) => {
-    console.log({ bus })
-    return (
-        <Paper variant="outlined" className={classes.busPaper}>
-            <Grid container spacing={2}>
-                <Grid item xs={12} md={2}>
-                    <img src={avatar} alt="avatar" className={classes.avatar} />
-                    <Typography variant="subtitle2">Network Travels</Typography>
-                </Grid>
-                <Grid item xs={6} md={2}>
-                    <Grid container direction="row">
-                        <Grid item xs={12}>
-                            <DirectionsBus />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Typography variant="h6">{bus.startTime}</Typography>
-                            <Typography variant="caption">{bus.fromCity.toUpperCase()}</Typography>
-                        </Grid>
-                    </Grid>
-                </Grid>
-                <Grid item xs={6} md={2}>
-                    <Grid container direction="row">
-                        <Grid item xs={12}>
-                            <DirectionsBus />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Typography variant="h6">{bus.endtime}</Typography>
-                            <Typography variant="caption">{bus.toCity.toUpperCase()}</Typography>
-                        </Grid>
-                    </Grid>
-                </Grid>
-                <Grid item xs={6} md={2}>
-                    <Grid container direction="row">
-                        <Grid item xs={12}>
-                            <AccessTime />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Typography variant="h5">{bus.travelTime}</Typography>
-                        </Grid>
-                    </Grid>
-                </Grid>
-                <Grid item xs={6} md={2}>
-                    <Typography variant="h4">&#8377;{bus.fare}</Typography>
-                </Grid>
-                <Grid item xs={12} md={2}>
-                    <Button variant="contained" onClick={() => handleClick({
-                        busId: bus.busId,
-                        midId: midId,
-                        startTime: bus.startTime,
-                        endtime: bus.endtime,
-                        seatCapacity: bus.seatCapacity,
-                        fare: bus.fare
-                    })} color="primary">Select Seat</Button>
-                </Grid>
+  return (
+    <Paper variant="outlined" className={classes.busPaper}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={2} className={classes.textAlignCenter}>
+          <img src={avatar} alt="avatar" className={classes.avatar} />
+          <Typography variant="subtitle2">Network Travels</Typography>
+        </Grid>
+        <Grid item xs={6} md={2} className={classes.textAlignCenter}>
+          <Grid container direction="row">
+            <Grid item xs={12} className={classes.textAlignCenter}>
+              <DirectionsBus />
             </Grid>
-        </Paper >
-    )
-}
+            <Grid item xs={12}>
+              <Typography>{bus.startTime}</Typography>
+              <Typography variant="caption">
+                {bus.fromCity.toUpperCase()}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={6} md={2} className={classes.textAlignCenter}>
+          <Grid container direction="row">
+            <Grid item xs={12}>
+              <DirectionsBus />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography>{bus.endtime}</Typography>
+              <Typography variant="caption">
+                {bus.toCity.toUpperCase()}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={6} md={2} className={classes.textAlignCenter}>
+          <Grid container direction="row">
+            <Grid item xs={12}>
+              <AccessTime />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography>{bus.travelTime}</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={6} md={2}>
+          <Typography variant="h5">&#8377;{bus.fare}</Typography>
+        </Grid>
+        <Grid item xs={12} md={2}>
+          <Button
+            variant="contained"
+            onClick={() =>
+              handleClick({
+                busId: bus.busId,
+                midId: midId,
+                startTime: bus.startTime,
+                endtime: bus.endtime,
+                seatCapacity: bus.seatCapacity,
+                fare: bus.fare
+              })
+            }
+            color="primary"
+          >
+            Select Seat
+          </Button>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
 
 export default withStyles(Styles)(BusCard);
--- src/containers/SearchResultContainer/SearchResult.js
+++ src/containers/SearchResultContainer/SearchResult.js
@@ -11,31 +11,37 @@ import { useDispatch } from "react-redux";
 import Constants from "../../redux/actionConstants";
 
 function SearchResult(props) {
-    let { classes } = props;
-    let data = { from: "Guwahati", to: "Tinsukia", noOfBuses: 0, noOfPassengers: 1, date: new Date().toDateString() }
-    let [response, setResponse] = useState([]);
-    let dispatch = useDispatch();
+  let { classes } = props;
+  let data = {
+    from: "Guwahati",
+    to: "Tinsukia",
+    noOfBuses: 0,
+    noOfPassengers: 1,
+    date: new Date().toDateString()
+  };
+  let [response, setResponse] = useState([]);
+  let dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch({
-            type: Constants.SET_SEARCH,
-            payload: {
-                from: data.from,
-                to: data.to,
-                date: data.date
-            }
-        });
-        setResponse(dummyResponse);
-    }, [])
-    return (
-        <div className={classes.gridstyle}>
-            <NavBar />
-            <BookingTitle data={data} />
-            <Form data={data} />
-            <ContentArea response={response} data={data} />
-            <Footer />
-        </div>
-    )
+  useEffect(() => {
+    dispatch({
+      type: Constants.SET_SEARCH,
+      payload: {
+        from: data.from,
+        to: data.to,
+        date: data.date
+      }
+    });
+    setResponse(dummyResponse);
+  }, []);
+  return (
+    <div className={classes.gridstyle}>
+      <NavBar />
+      <BookingTitle data={data} />
+      {/* <Form data={data} /> */}
+      <ContentArea response={response} data={data} />
+      <Footer />
+    </div>
+  );
 }
 
 export default withStyles(Styles)(SearchResult);
