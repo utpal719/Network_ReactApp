ccccc3e5079b355f0c8c716f87cadf0110b2994f
Samudra Deka <samudra@techvariable.com>
2020-06-16 19:16:21 +0530

    ETicket reverted to old format

--- src/containers/BusChart/PassengerTable.js
+++ src/containers/BusChart/PassengerTable.js
@@ -7,10 +7,10 @@ import {
   TableCell,
   TableBody,
   Card,
-  Box
+  Box,
 } from "@material-ui/core";
 
-const PassengerTable = props => {
+const PassengerTable = (props) => {
   const { classes, list } = props;
   const columns = [
     { column: "Seat No" },
@@ -19,7 +19,7 @@ const PassengerTable = props => {
     { column: "Mobile" },
     { column: "PNR Number" },
     { column: "Destination" },
-    { column: "User" }
+    { column: "User" },
   ];
   const innerList = [{ column: "seatNumber" }, { column: "passengerName" }];
   const outerList = [
@@ -27,43 +27,82 @@ const PassengerTable = props => {
     { column: "mobile" },
     { column: "pnrNumber" },
     { column: "toCity" },
-    { column: "userName" }
+    { column: "userName" },
   ];
   return (
-    <Box displayPrint="block">
-      <Card>
+    <Card>
+      <Box displayPrint="none">
         <Table className={classes.table}>
           <TableRow className={classes.tablehead}>
-            {columns.map(item => {
+            {columns.map((item) => {
               return <TableCell>{item.column}</TableCell>;
             })}
           </TableRow>
           {/* </TableHead> */}
-            {list.length ? (
-              list.map(item => {
-                return item.passengerList.map(header => {
-                  return (
-                    <TableRow className={classes.tablerow}>
-                      {innerList.map(il => {
-                        return <TableCell>{header[il.column]}</TableCell>;
-                      })}
-                      {outerList.map(ol => {
-                        return <TableCell>{item[ol.column]}</TableCell>;
-                      })}
-                    </TableRow>
-                  );
-                });
-              })
-            ) : (
-              <TableRow className={classes.tablerow}>
-                <TableCell colSpan={columns.length} align="center">
-                  No Records found
-                </TableCell>
-              </TableRow>
-            )}
+          {list.length ? (
+            list.map((item) => {
+              return item.passengerList.map((header) => {
+                return (
+                  <TableRow className={classes.tablerow}>
+                    {innerList.map((il) => {
+                      return <TableCell>{header[il.column]}</TableCell>;
+                    })}
+                    {outerList.map((ol) => {
+                      return <TableCell>{item[ol.column]}</TableCell>;
+                    })}
+                  </TableRow>
+                );
+              });
+            })
+          ) : (
+            <TableRow className={classes.tablerow}>
+              <TableCell colSpan={columns.length} align="center">
+                No Records found
+              </TableCell>
+            </TableRow>
+          )}
         </Table>
-      </Card>
-    </Box>
+      </Box>
+      <Box display="none" displayPrint="block">
+        <table style={{ width: "100%" }}>
+          <tr style={{ background: "#8e8e8e", color: "#fff" }}>
+            <td style={{ padding: "5px 10px" }}>Seat No</td>
+            <td style={{ padding: "5px 10px" }}>Passenger Name</td>
+            <td style={{ padding: "5px 10px" }}>Boarding Point</td>
+            <td style={{ padding: "5px 10px" }}>Mobile</td>
+            <td style={{ padding: "5px 10px" }}>PNR Number</td>
+            <td style={{ padding: "5px 10px" }}>Destination</td>
+            <td style={{ padding: "5px 10px" }}>User</td>
+          </tr>
+          {list.length ? (
+            list.map((item) => {
+              return item.passengerList.map((header) => {
+                return (
+                  <tr className={classes.tablerow}>
+                    {innerList.map((il) => {
+                      return (
+                        <td style={{ padding: "5px 10px",fontSize : "16px" }}>
+                          {header[il.column]}
+                        </td>
+                      );
+                    })}
+                    {outerList.map((ol) => {
+                      return <td>{item[ol.column]}</td>;
+                    })}
+                  </tr>
+                );
+              });
+            })
+          ) : (
+            <tr className={classes.tablerow}>
+              <td colSpan={columns.length} align="center">
+                No Records found
+              </td>
+            </tr>
+          )}
+        </table>
+      </Box>
+    </Card>
   );
 };
 
